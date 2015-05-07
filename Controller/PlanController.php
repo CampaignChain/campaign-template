2b346549c894418326c3913432f81608ffe62e79
Sandro Groganz <sandro@campaignchain.com>
2015-05-07 10:17:07 -0700

    initial commit

---
+++ Controller/PlanController.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) Sandro Groganz <sandro@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CampaignChain\Campaign\TemplateBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PlanController extends Controller
+{
+    const BUNDLE_NAME = 'campaignchain/campaign-template';
+    const MODULE_IDENTIFIER = 'campaignchain-template';
+
+    public function timelineDetailAction(Request $request, $id){
+        /*
+         * Set current campaign in session, e.g. to pre-fill the campaign field
+         * in a new activity with it.
+         */
+        $this->get('session')->set('campaignchain.campaign', $id);
+
+        return $this->render(
+            'CampaignChainCampaignTemplateBundle:Plan/Timeline:detail.html.twig',
+            array(
+                'page_title' => 'Plan Campaign Template',
+                'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(
+                        self::BUNDLE_NAME, self::MODULE_IDENTIFIER, $id
+                    ),
+                'gantt_toolbar_status' => 'default',
+                'path_embedded' => '',
+                'path_fullscreen' =>  '',
+            ));
+    }
+
+//    public function timelineAction(){
+//        return $this->render(
+//            'CampaignChainCoreBundle:Plan/Timeline:index.html.twig',
+//            array(
+//                'page_title' => 'Plan Campaign Templates',
+//                'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(),
+//                'gantt_toolbar_status' => 'default',
+//                'path_embedded' => $this->generateUrl('campaignchain_campaign_template_plan_timeline'),
+//                'path_fullscreen' =>  $this->generateUrl('campaignchain_campaign_template_plan_timeline_fullscreen'),
+//            ));
+//    }
+//
+//    public function timelineFullScreenAction(){
+//        return $this->render(
+//            'CampaignChainCoreBundle:Plan/Timeline:fullscreen.html.twig',
+//            array(
+//                'page_title' => 'Plan Campaign Templates',
+//                'gantt_tasks' => $this->get('campaignchain.core.model.dhtmlxgantt')->getTasks(),
+//                'gantt_toolbar_status' => 'fullscreen',
+//                'path_fullscreen_close' => $this->generateUrl('campaignchain_campaign_template_plan_timeline'),
+//                'path_fullscreen' =>  $this->generateUrl('campaignchain_campaign_template_plan_timeline_fullscreen'),
+//            ));
+//    }
+}

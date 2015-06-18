2a6c7b54a88a213bc587daded3d24e3ec82b6ef4
Sandro Groganz <sandro@campaignchain.com>
2015-06-18 13:27:42 +0200

    CE-299 Campaign template

---
+++ EntityService/CampaignTemplateService.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) Sandro Groganz <sandro@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CampaignChain\Campaign\TemplateBundle\EntityService;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use CampaignChain\CoreBundle\Entity\Action;
+
+class CampaignTemplateService
+{
+    protected $em;
+    protected $container;
+
+    public function __construct(EntityManager $em, ContainerInterface $container)
+    {
+        $this->em = $em;
+        $this->container = $container;
+    }
+
+    public function processAction(Action $action){
+        $action->setStatus(Action::STATUS_PAUSED);
+        return $action;
+    }
+}
